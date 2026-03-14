d753ae4261d97d99e80a7291348ad2a9973817ba
ayush392 <ayushgoyal.0004@gmail.com>
2026-03-15 00:06:45 +0530

    feat: trim whitespace in schema validations; update transaction types for staff transactions

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -5,18 +5,16 @@ const { TransactionTypeEnum } = Transaction;
 // applicable for staff only
 export const STAFF_ONLY_TRANSACTION_TYPES: readonly TransactionType[] = [
   TransactionTypeEnum.enum.SALARY,
-  TransactionTypeEnum.enum.BONUS
+  TransactionTypeEnum.enum.ADVANCE
 ];
 export const TRANSACTION_EFFECT: Record<TransactionType, number> = {
   // do not affect dues/outstanding balance
   [TransactionTypeEnum.enum.SALARY]: 0,
-  [TransactionTypeEnum.enum.BONUS]: 0,
 
   // increase the dues/outstanding balance
   [TransactionTypeEnum.enum.UDHAAR]: 1,
+  [TransactionTypeEnum.enum.ADVANCE]: 1,
 
   // decrease the dues/outstanding balance
-  [TransactionTypeEnum.enum.PAYMENT]: -1,
-  [TransactionTypeEnum.enum.ADJUSTMENT]: -1,
-  [TransactionTypeEnum.enum.RETURN]: -1
+  [TransactionTypeEnum.enum.PAYMENT]: -1
 };
